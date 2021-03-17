9247403401410cea223e086dc293803a45a59024
soorajksmkd <soorajksmkd@gmail.com>
2021-03-17 19:30:42 +0530

    second commit

--- src/main/java/com/infosys/project/user/validator/ProductValidator.java
+++ src/main/java/com/infosys/project/user/validator/ProductValidator.java
@@ -2,10 +2,10 @@ package com.infosys.project.user.validator;
 
 import java.math.BigDecimal;
 
-import org.hibernate.cfg.Environment;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+
 
 import com.infosys.project.user.dto.ProductDTO;
 
@@ -14,8 +14,7 @@ public class ProductValidator {
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	
-	@Autowired
-	Environment environment;
+	
 
 	public static String validateProduct(ProductDTO productDTO) {
 		if (!isValidProductName(productDTO.getProductName()))
@@ -26,19 +25,12 @@ public class ProductValidator {
 			return ("Product should be priced atleast at ");
 		if (!isValidStock(productDTO.getStock()))
 			return ("Stock should be minimum 10!!");
-		if (!isValidImage(productDTO.getImage()))
-			return ("Image should be in jpeg/png format!!!");
+	
 		return ("OK");
 
 	}
 
-	private static boolean isValidImage(String image) {
-		String imageSubString = image.substring(1, image.length()-1);
-		String regex= "[^\s]+.(jpeg|png)";
-		if (imageSubString.matches(regex))
-			return true;
-		return false;
-	}
+	
 
 	private static boolean isValidStock(Long stock) {
 		if (stock >= 10L)
